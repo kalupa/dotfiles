bc97218c72a17bdd0120f688eda2f91894486d05
Paul Kalupnieks <paul@unbounce.com>
2015-08-10 14:53:03 -0700

    Updates

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -12,17 +12,16 @@ printf "\n"
 
 echo "Installing tools"
 brew update
-brew install ack ag apple-gcc42 atk casperjs colordiff coreutils ctags gnutls
-brew install gnu-typist gpg-agent imagemagick jpeg jq markdown meld mercurial
-brew install mysql openssl parallel pt python rbenv ruby-build tree tmux
-brew install tmux-mem-cpu-load vim watchman zsh jenv
-brew install emacs-mac --with-spacemacs-icon --with-xml2 --with-gnutls --with-ctags --with-imagemagick --with-glib
+brew install ack ag apple-gcc42 autojump atk casperjs colordiff coreutils ctags gnutls
+brew install gnu-typist gpg-agent imagemagick jenv jpeg jq markdown meld mercurial
+brew install mysql node openssl parallel pt python rbenv ruby-build tree tmux
+brew install tmux-mem-cpu-load vim watchman zsh
 
 # brew install flow
 printf "\n"
 
 echo "Installing tools with options"
-#brew install emacs --with-cocoa --with-imagemagick --with-gnutls
+brew install emacs-mac --with-spacemacs-icon --with-xml2 --with-gnutls --with-ctags --with-imagemagick --with-glib
 brew install git --with-brewed-curl --with-brewed-openssl --with-gettext --with-pcre --with-blk-sha1
 brew install gnugo2 --with-readline
 brew install reattach-to-user-namespace  --with-wrap-launchctl --with-wrap-pbcopy-and-pbpaste
