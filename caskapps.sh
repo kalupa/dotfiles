9bf39557d48fd8f9741dffa5a9b58e1fff6535bc
Paul Kalupnieks <paul@unbounce.com>
2016-06-14 15:09:13 -0700

    Removes apps that are crappy to install from cask

--- caskapps.sh
+++ caskapps.sh
@@ -5,9 +5,9 @@ brew install caskroom/cask/brew-cask
 
 brew update
 echo "Installing mac apps with cask"
-brew cask install 1password atom bittorrent-sync
-brew cask install chrome-devtools dash diffmerge evernote-mac fantastical
-brew cask install firefox flashlight flux gitup
+brew cask install atom bittorrent-sync
+brew cask install dash diffmerge
+brew cask install flashlight flux gitup
 brew cask install google-chrome gpgtools iterm2 karabiner kdiff3
-brew cask install macdown spotify seil
-brew cask install sequel-pro slack soulver spideroak things virtualbox vlc
+brew cask install macdown seil
+brew cask install sequel-pro virtualbox vlc
