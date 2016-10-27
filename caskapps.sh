7d264e65c6dd1e2abf80906d6daa4a15302af015
Paul Kalupnieks <paul@unbounce.com>
2016-10-27 13:28:16 -0700

    Updates spacemacs for newest release

--- caskapps.sh
+++ caskapps.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 echo "Installing caskroom"
-brew install caskroom/cask/brew-cask
+# brew install caskroom/cask/brew-cask
 
 brew update
 echo "Installing mac apps with cask"
-brew cask install atom bittorrent-sync
-brew cask install dash diffmerge
-brew cask install flashlight flux gitup
-brew cask install google-chrome gpgtools iterm2 karabiner kdiff3
-brew cask install macdown seil
-brew cask install sequel-pro virtualbox vlc
+# brew cask install bittorrent-sync dash flux gitup google-chrome gpgtools iterm2 sequel-pro vlc
