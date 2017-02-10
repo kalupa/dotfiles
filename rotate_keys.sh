2d802fef4aa2612e9f71d5879210a192000395dd
Paul Kalupnieks <paul@unbounce.com>
2017-02-09 17:44:37 -0800

    Update rotate aws keys

--- rotate_keys.sh
+++ rotate_keys.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-echo "******** HOW TO ROTATE AWS KEYS *******"
-echo ""
-echo "$ aws iam create-access-key"
-echo "NOTE: outputs JSON object with new credentials. Update .aws/env.sh and .aws/credentials"
-echo ""
-echo "$ aws iam list-access-keys"
-echo "NOTE: Find oldest key for deletion"
-echo ""
-echo "$ aws iam delete-access-key --access-key-id KEY_ID/\$AWS_ACCESS_KEY_ID"
+aws-rotate-keys
