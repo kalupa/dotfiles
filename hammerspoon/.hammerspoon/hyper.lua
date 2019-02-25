c500e46ef208a46743f1dc95938ae3d30e605e61
Paul Kalupnieks <paul@karrupa.com>
2019-02-25 10:32:13 -0800

    go full macos for hyper

--- hammerspoon/.hammerspoon/hyper.lua
+++ hammerspoon/.hammerspoon/hyper.lua
@@ -4,14 +4,17 @@
 
 -- Keybindings for launching apps in Hyper Mode
 hyperModeAppMappings = {
+  { 'b', 'Google Chrome' }, -- Work B-rowser
   { 'c', 'Sblack' }, -- Work C-hat
   { 'd', 'OmniFocus' }, -- to-D-o
   { 'e', '/Applications/Emacs.app' }, -- Text Editor
+  { 'm', 'Mail'}, -- Mail
   { 'r', 'Visual Studio Code' }, -- vscode
   { 't', 'iTerm' }, -- T-erminal
-  { 'm', 'Airmail 3'}, -- Mail
+  { ';', 'Safari' }, -- Work B-rowser
 }
 
+-- { 'm', 'Airmail 3'}, -- Mail
 -- { 'm', 'Canary Mail'}, -- Mail
 -- { 'p', 'Franz' }, -- Personal Chat
 -- { 'c', 'Slack' }, -- Work C-hat
@@ -22,26 +25,25 @@ hyperModeAppMappings = {
 -- { 't', 'Hyper' }, -- T-erminal
 -- { 'k', 'Fantastical 2' }, -- K-alendar
 
+
+local _HYPER = {'cmd,ctrl,alt,shift'}
+
 for i, mapping in ipairs(hyperModeAppMappings) do
-  hs.hotkey.bind({'cmd,ctrl,alt,shift'}, mapping[1], function()
-    hs.application.launchOrFocus(mapping[2])
+  local key = mapping[1]
+  local appName = mapping[2]
+
+  hs.hotkey.bind(_HYPER, key, function()
+    hs.application.launchOrFocus(appName)
   end)
 end
 
--- -- { 'b', 'Google Chrome' }, -- Work B-rowser
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, "b", function()
-    switchToChromeProfile('U')
-end)
+hs.hotkey.bind(_HYPER, "k", function() switchToCalendar() end)
 
--- -- { ';', 'Brave Browser' }, -- Personal browsing
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, ";", function()
-    -- switchToChromeProfile('Paul')
-    hs.application.launchOrFocus('Firefox')
-end)
+-- hs.hotkey.bind(_HYPER, "b", function()
+--     switchToChromeProfile('U')
+-- end)
 
-fullscreen = function()
-  hs.grid.maximizeWindow(hs.window.focusedWindow())
-end
+fullscreen = function() hs.grid.maximizeWindow(hs.window.focusedWindow()) end
 
 -- Set some resize grids up
 hs.grid.setGrid('10x3') -- default to something that maps nicely ...
@@ -49,56 +51,57 @@ hs.grid.setGrid('8x3', '1680x1050')
 hs.grid.setMargins({0,0}) -- gapless
 hs.window.animationDuration = 0 -- disable animations
 
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, '=', hs.caffeinate.startScreensaver)
+hs.hotkey.bind(_HYPER, '=', hs.caffeinate.startScreensaver)
 
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, 'g', hs.grid.toggleShow)
+hs.hotkey.bind(_HYPER, 'g', hs.grid.toggleShow)
 
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, "space", fullscreen) -- BAM!
+hs.hotkey.bind(_HYPER, "space", fullscreen) -- BAM!
 
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, "Left", function()
+hs.hotkey.bind(_HYPER, "Left", function()
     local win = hs.window.focusedWindow()
     win:moveOneScreenWest(false, true, 0)
 end)
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, "Right", function()
+hs.hotkey.bind(_HYPER, "Right", function()
     local win = hs.window.focusedWindow()
     win:moveOneScreenEast(false, true, 0)
 end)
 
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, "[", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+hs.hotkey.bind(_HYPER, "[", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
 
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
-    win:setFrame(f, 0)
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f, 0)
 end)
 
-hs.hotkey.bind({'cmd,ctrl,alt,shift'}, "]", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+hs.hotkey.bind(_HYPER, "]", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
 
-    f.x = max.x + (max.w / 2)
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
-    win:setFrame(f, 0)
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f, 0)
 end)
 
 
--- hs.hotkey.bind({'cmd,ctrl,alt,shift'}, "k", function()
---     hs.osascript.applescript([[
---       tell application "Calendar"
---       switch view to day view
---       view calendar at current date
---       end tell
---       ]])
--- end)
+switchToCalendar = function()
+  hs.osascript.applescript([[
+    tell application "Calendar"
+    switch view to day view
+    view calendar at current date
+    end tell
+    ]])
+end
+
 
 switchToChromeProfile = function(profileName)
   chromeSwitcher = 'set PROFILE to "' .. profileName .. '"' .. [[
