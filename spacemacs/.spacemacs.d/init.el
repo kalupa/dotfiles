a767ab3cea28f392e2e305999a7f00c3c6e41d9f
Paul Kalupnieks <paul@karrupa.com>
2019-04-03 14:04:50 -0700

    Adds Brewfile
    - update fonts list

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -240,8 +240,14 @@ It should only modify the values of Spacemacs settings."
 
     ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
     ;; quickly tweak the mode-line size to make separators look not too crappy.
+    ;; dotspacemacs-default-font
+    ;; '("FuraCode Nerd Font"
+    ;;    :size 12
+    ;;    :weight normal
+    ;;    :width normal
+    ;;    )
     dotspacemacs-default-font
-    '("FuraCode Nerd Font"
+    '("Hack Nerd Font"
        :size 12
        :weight normal
        :width normal
